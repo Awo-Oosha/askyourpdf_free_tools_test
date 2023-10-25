6b5746b8de8faa632b6c8d843722d9f59165e533
Roland Umukoro <developement.rcode@gmail.com>
2023-10-25 20:50:48 +0100

    fix

--- declarations.d.ts
+++ declarations.d.ts
@@ -10,3 +10,21 @@ declare module "*.svg" {
   declare module "*.gif";
   declare module "get-browser-fingerprint";
   declare module "*.svg?url";
+  declare module "react-copy-to-clipboard" {
+    import React from "react";
+  
+    interface Options {
+        debug: boolean;
+        message: string;
+    }
+  
+    interface Props {
+        text: string;
+        onCopy?(a: string, b: boolean): void;
+        options?: Options;
+        children?: ReactNode
+    }
+  
+    class CopyToClipboard extends React.Component<Props, {}> {}
+    export default CopyToClipboard;
+  }
