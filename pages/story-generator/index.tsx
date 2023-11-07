4cc2134a1ae71dd91e1f4d8e4fc63e670fa58995
Samagent97 <sammccarthy090@gmail.com>
2023-11-07 13:28:53 +0100

    chore : create story generator page

---
+++ pages/story-generator/index.tsx
@@ -0,0 +1,103 @@
+import React, {ChangeEvent, useEffect, useRef, useState} from "react";
+import {Layout} from "antd";
+import {Content} from "antd/lib/layout/layout";
+import styled from "styled-components";
+import LyricsGen from "@/img/tools/lyricsGen.png";
+import {alerts} from "@/utils/alerts";
+import {t, Trans} from "@lingui/macro";
+import {GetStaticPaths, GetStaticProps} from "next";
+import {loadCatalog} from "@/utils/i18n";
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import FAQ from "@/components/FAQ";
+import Footer from "@/components/Footer";
+import {useMedia} from "react-use";
+import HeroImage from "@/img/Mask.svg?url";
+import {PAGE_DESCRIPTION, PAGE_TITLE, path} from "@/routes";
+import {MAIN_APP_URL} from "@/config/config";
+import {SourceContent} from "@/components/source-tools/source-content";
+import {SourceResult} from "@/components/source-tools/source-result";
+import {useMutation} from "react-query";
+import {Filter, getSourceInformation, getSources} from "@/services/tools";
+import {useRouter} from "next/router";
+import ProgressModal from "@/components/Modals/ProgressModal";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const translation = await loadCatalog(ctx.locale!);
+    return {
+        props: {
+            translation,
+            description: PAGE_DESCRIPTION[path.sourceTool],
+            canonicalUrl: `${MAIN_APP_URL}${path.sourceTool}`,
+            title: PAGE_TITLE[path.sourceTool],
+            imageUrl: "/img/AI-story-generator.svg"
+        },
+    };
+};
+
+const StoryGenerator = () => {
+    const router = useRouter();
+    const options:any = [
+        {name:"Select Genre",data:[
+            {label:"Story",key:"str",  onClick:(key:any)=>{}},
+            {label:"Story2",key:"str2",onClick:(key:any)=>{}},
+            {label:"Story3",key:"str3",onClick:(key:any)=>{}},
+        ]},
+        {name:"Select Length",data:[
+            {label:"Story",key:"str",  onClick:(key:any)=>{}},
+            {label:"Story2",key:"str2",onClick:(key:any)=>{}},
+            {label:"Story3",key:"str3",onClick:(key:any)=>{}},
+        ]}
+        ,
+        {name:"Select Settings",data:[
+            {label:"Story",key:"str",  onClick:(key:any)=>{}},
+            {label:"Story2",key:"str2",onClick:(key:any)=>{}},
+            {label:"Story3",key:"str3",onClick:(key:any)=>{}},
+        ]}
+    ];
+    const textfields:any=[
+        {placeholder:t`Input Names and a brief description of the main characters`,height:"90px"},
+    ];
+
+    const textfield:any=[
+        {placeholder:t`Input Key events/plot points`,height:"90px"},
+    ];
+    const faqs=[
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+        {question:"Is there a free trial available?",answer:"Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible."},
+    ]
+    return (<div>
+        <Navbar/>
+        <Hero 
+        image={LyricsGen} 
+        title={t`AI Story Generator`} 
+        description={t`Fill`}
+        fields={textfields }
+        buttonText={"Generate Story"}
+        buttonFunction={(text:any,items:any)=>{
+            console.log(text,items);
+        }}
+        selectOptions={options} 
+        />
+
+        
+        <FAQ data={faqs}/>
+        <Footer/>
+
+    </div>);
+ 
+};
+
+export default StoryGenerator;
